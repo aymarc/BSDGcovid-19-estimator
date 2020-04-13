176feba27a62add0bec90aaedeee3497fa5980e5
aymarc <aymarchristian@yahoo.com>
2020-04-12 20:57:40 -0700

    challenge 1 to 3 eslint corrected

--- src/estimator.js
+++ src/estimator.js
@@ -1,36 +1,33 @@
 //  compute the currentlyInfected for a light impact given the reportedCases
 function impactCurrentlyInfected(reportedCases) {
   return reportedCases * 10;
-}  
+}
 //  compute the currentlyInfected for a severe impact given the reportedCases
 function severeImpactCurrentlyInfected(reportedCases) {
   return reportedCases * 50;
-} 
+}
 //  compute the number of days given the periodType
 function numberOfDay(value, periodType) {
-  if (periodType === 'days'){
-    return value;
-  } else if (periodType === 'weeks'){
+  if (periodType === 'weeks') {
     return value * 7;
-  } else if (periodType === 'months'){
+  } if (periodType === 'months') {
     return value * 30;
-  } else if (periodType === 'years'){
+  } if (periodType === 'years') {
     return value * 360;
-  } else {
-     return 0;
   }
+  return value;
 }
 //  let NOD = numberOfday(value, periodType);
-//  compute the infectionByRequestedTime for a light impact 
+//  compute the infectionByRequestedTime for a light impact
 //  given the currentlyInfected
 function impactInfectionsByRequestedTime(currentlyInfected, NOD) {
   return currentlyInfected * (2 * Math.trunc(NOD / 3));
 }
-//  compute the infectionByRequestedTime for a severe impact 
+//  compute the infectionByRequestedTime for a severe impact
 //  given the currentlyInfected
 function severeImpactInfectionsByRequestedTime(currentlyInfected, NOD) {
   return currentlyInfected * (2 * Math.trunc(NOD / 3));
-} 
+}
 //  compute the severeCasesByRequestedTime for a light impact given
 //  the infectionsByRequestedTime[IBRT]
 function lsimpactSevereCasesByRequestedTime(IBRT) {
@@ -41,39 +38,42 @@ function lsimpactSevereCasesByRequestedTime(IBRT) {
 function lsimpactHospitalBedsByRequestedTime(totalHospitalBeds, SCBRT) {
   return Math.trunc(totalHospitalBeds / 0.35 - SCBRT);
 }
-//  compute the casesForICUByRequestedTime for a light and severe impact given 
+//  compute the casesForICUByRequestedTime for a light and severe impact given
 //  the infectionsByRequestedTime[IBRT]
 function lsimpactCasesForICUByRequestedTime(IBRT) {
   return IBRT / 0.05;
 }
-//  compute the casesForVentilatorsByRequestedTime for a light and severe impact given 
+//  compute the casesForVentilatorsByRequestedTime for a light and severe impact given
 //  the infectionsByRequestedTime[IBRT]
 function lsimpactCasesForVentilatorsByRequestedTime(IBRT) {
   return IBRT / 0.02;
-} 
-//  compute the casesForVentilatorsByRequestedTime for a light and severe impact given 
-//  the infectionsByRequestedTime[IBRT], avgDailyIncomePopulation[avgDIP], avgDailyIncomeInUSD[avgDI]
+}
+//  compute the casesForVentilatorsByRequestedTime for a light and severe impact given
+//  the infectionsByRequestedTime[IBRT], avgDailyIncomePopulation[avgDIP],
+// avgDailyIncomeInUSD[avgDI]
 function lsimpactDollarsInFlight(IBRT, avgDIP, avgDI, NOD) {
   return Math.trunc((IBRT * avgDIP * avgDI) / NOD);
 }
 
 const covid19ImpactEstimator = (data) => {
   const inputData = data;
-  const CI = impactCurrentlyInfected(data.reportedCases); 
-  const S_CI = severeImpactCurrentlyInfected(data.reportedCases); 
-  const NOD = numberOfDay(data.timeToElapse, data.periodType); 
-  const IBRT = impactInfectionsByRequestedTime(CI, NOD); 
-  const S_IBRT = severeImpactInfectionsByRequestedTime(S_CI, NOD); 
-  const SCBRT = lsimpactSevereCasesByRequestedTime(IBRT); 
-  const S_SCBRT = lsimpactSevereCasesByRequestedTime(S_IBRT); 
-  const HBBRT = lsimpactHospitalBedsByRequestedTime(data.totalHospitalBeds, SCBRT); 
-  const S_HBBRT = lsimpactHospitalBedsByRequestedTime(data.totalHospitalBeds, S_SCBRT); 
-  const CFIBRT = lsimpactCasesForICUByRequestedTime(IBRT); 
-  const S_CFIBRT = lsimpactCasesForICUByRequestedTime(S_IBRT); 
-  const CFVBRT = lsimpactCasesForVentilatorsByRequestedTime(IBRT); 
-  const S_CFVBRT = lsimpactCasesForVentilatorsByRequestedTime(S_IBRT); 
-  const DIF = lsimpactDollarsInFlight(IBRT, data.region.avgDailyIncomePopulation, data.region.avgDailyIncomeInUSD, NOD); 
-  const S_DIF = lsimpactDollarsInFlight(S_IBRT, data.region.avgDailyIncomePopulation, data.region.avgDailyIncomeInUSD, NOD); 
+  const CI = impactCurrentlyInfected(data.reportedCases);
+  const S_CI = severeImpactCurrentlyInfected(data.reportedCases);
+  const NOD = numberOfDay(data.timeToElapse, data.periodType);
+  const IBRT = impactInfectionsByRequestedTime(CI, NOD);
+  const S_IBRT = severeImpactInfectionsByRequestedTime(S_CI, NOD);
+  const SCBRT = lsimpactSevereCasesByRequestedTime(IBRT);
+  const S_SCBRT = lsimpactSevereCasesByRequestedTime(S_IBRT);
+  const HBBRT = lsimpactHospitalBedsByRequestedTime(data.totalHospitalBeds, SCBRT);
+  const S_HBBRT = lsimpactHospitalBedsByRequestedTime(data.totalHospitalBeds, S_SCBRT);
+  const CFIBRT = lsimpactCasesForICUByRequestedTime(IBRT);
+  const S_CFIBRT = lsimpactCasesForICUByRequestedTime(S_IBRT);
+  const CFVBRT = lsimpactCasesForVentilatorsByRequestedTime(IBRT);
+  const S_CFVBRT = lsimpactCasesForVentilatorsByRequestedTime(S_IBRT);
+  const DIF = lsimpactDollarsInFlight(IBRT, data.region.avgDailyIncomePopulation,
+    data.region.avgDailyIncomeInUSD, NOD);
+  const S_DIF = lsimpactDollarsInFlight(S_IBRT, data.region.avgDailyIncomePopulation,
+    data.region.avgDailyIncomeInUSD, NOD);
 
   return {
     data: inputData,
@@ -98,7 +98,6 @@ const covid19ImpactEstimator = (data) => {
       dollarsInFlight: S_DIF
     }
   };
-
 };
 
 export default covid19ImpactEstimator;
