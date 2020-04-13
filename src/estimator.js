a27ac4bebd3f35628e59b45028e89e414ea583cd
aymarc <aymarchristian@yahoo.com>
2020-04-12 19:22:05 -0700

    challenge 1 to 3 updated

--- src/estimator.js
+++ src/estimator.js
@@ -129,19 +129,19 @@ const covid19ImpactEstimator = (data) => {
 };
 
     
-data ={
-    region:{
-        name:"Africa",
-        avgAge: 19.7,
-        avgDailyIncomeInUSD: 5,
-        avgDailyIncomePopulation: 0.71
-    },
-    periodType: "days",
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-}
+// data ={
+//     region:{
+//         name:"Africa",
+//         avgAge: 19.7,
+//         avgDailyIncomeInUSD: 5,
+//         avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: "days",
+//     timeToElapse: 58,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+// }
 
 // let testRun = covid19ImpactEstimator(data);
 // console.log(testRun);
